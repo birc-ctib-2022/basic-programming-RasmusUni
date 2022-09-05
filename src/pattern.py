c78b25a7b10cdc81a83463c15257a008ae1e47da
RasmusUni <202004598@post.au.dk>
2022-09-05 15:13:53 +0200

    pattern

--- src/pattern.py
+++ src/pattern.py
@@ -2,12 +2,12 @@
 # Print the pattern
 list2 = []
 
-for x in range(10):
+for x in range(9):
     if x <=4:
         list2.append("*")
         y=" ".join(list2)
         print(y)
-    elif x>4:
+    elif x>0:
         list2.pop(0)
         y=" ".join(list2)
         print(y)
