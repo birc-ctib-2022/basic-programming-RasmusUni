9a6c697853e0cfc89987175ccc456b543b347748
RasmusUni <202004598@post.au.dk>
2022-09-06 11:16:45 +0200

    password

--- src/password.py
+++ src/password.py
@@ -9,4 +9,24 @@ is_valid = False
 
 # Do all the requirement checks here.
 
+hej=[0, 0, 0, 0, 0, 0]
+
+
+if len(password) >= 6:
+    hej[0]=1
+    if len(password) <= 16:
+        hej[1]=1
+        for x in password:
+            if x.isupper()==True:
+                hej[2]=1
+            elif x.islower()==True:
+                hej[3]=1
+            elif x.isnumeric()==True:
+                hej[4]=1
+            elif x in "$#@":
+                hej[5]=1
+
+if 0 not in hej:
+    is_valid = True
+
 print(is_valid)
